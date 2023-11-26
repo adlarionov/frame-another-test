bb5ca268092c6fb58ebb66c2c3dc789a35653a4a
payne3105 <payne3105@gmail.com>
2023-11-26 11:45:08 +0300

    test

--- src/components/Camera/Camera.tsx
+++ src/components/Camera/Camera.tsx
@@ -1,50 +1,75 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import Frame from "../Frame/Frame";
 
 export default function Camera() {
   const videoRef = useRef<HTMLVideoElement>(null);
-  const [startScanning, setStartScanning] = useState<boolean>(false);
+  // const [startScanning, setStartScanning] = useState<boolean>(false);
+  const [stream, setStream] = useState<MediaStream>();
 
-  const [constraints, setConstraints] = useState({
-    aspectRatio: 1,
-    facingMode: { exact: "environment" },
-  });
-  const [mediaStream, setMediaStream] = useState<MediaStream | null>(null);
+  const [facingMode, setFacingMode] = useState<string>("environment");
 
-  const stopScanning = () => {
-    if (mediaStream) {
-      mediaStream.getTracks().forEach((track) => track.stop());
+  const handleStartCamera = async () => {
+    console.log('here camera')
+    try {
+      if (stream) {
+        stream.getVideoTracks().forEach((track) => track.stop());
+      }
+      console.log('here camera 2', facingMode)
+      const mediaDevices = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: { facingMode, aspectRatio: 1 },
+      });
+      setStream(mediaDevices);
+    } catch (error) {
+      console.error(error);
+      return;
     }
-    setStartScanning(false);
+    if (videoRef.current && stream) {
+      videoRef.current.srcObject = stream;
+      videoRef.current.play();
+    }
   };
 
-  useEffect(() => {
-    const startScanningFunc = async () => {
-      const mediaDevices = await navigator.mediaDevices.getUserMedia({
-        video: constraints,
-      });
-
-      if (videoRef.current && mediaDevices) {
-        videoRef.current.srcObject = mediaDevices;
-        await videoRef.current.play().catch((error) => console.error(error));
-      }
-      setMediaStream(mediaDevices);
-    };
-    if (startScanning) {
-      startScanningFunc();
+  const changeFacingMode = () => {
+    if (facingMode === "user") {
+      setFacingMode("environment");
+    } else {
+      setFacingMode("user");
     }
-  }, [constraints, mediaStream, startScanning]);
+    handleStartCamera();
+  };
+
+  // useEffect(() => {
+  //   const startScanningFunc = async () => {
+  //     const mediaDevices = await navigator.mediaDevices.getUserMedia({
+  //       audio: false,
+  //       video: { aspectRatio: 1, facingMode },
+  //     });
+
+  //     if (videoRef.current && mediaDevices) {
+  //       videoRef.current.srcObject = mediaDevices;
+  //       await videoRef.current.play().catch((error) => console.error(error));
+  //     }
+  //   };
+
+  //   if (startScanning) {
+  //     console.log("inside use effect", facingMode);
+  //     startScanningFunc();
+  //   }
+  // }, [facingMode, startScanning]);
 
   return (
     <div>
-      <video
-        autoPlay
-        playsInline
-        preload="auto"
-        ref={videoRef}
-        width="99%"
-        height="500"
-      />
+      {stream && (
+        <video
+          autoPlay
+          playsInline
+          preload="auto"
+          ref={videoRef}
+          width="99%"
+          height="500"
+        />
+      )}
       {videoRef.current?.playsInline && <Frame video={videoRef.current} />}
       <div
         style={{
@@ -53,28 +78,10 @@ export default function Camera() {
           alignItems: "center",
         }}
       >
-        {startScanning && (
-          <button
-            onClick={() =>
-              setConstraints((prev) => {
-                return {
-                  aspectRatio: 1,
-                  facingMode: {
-                    exact:
-                      prev.facingMode.exact === "user" ? "environment" : "user",
-                  },
-                };
-              })
-            }
-          >
-            Facing Mode {constraints.facingMode.exact}
-          </button>
+        {stream && (
+          <button onClick={changeFacingMode}>Facing Mode {facingMode}</button>
         )}
-        <button
-          onClick={startScanning ? stopScanning : () => setStartScanning(true)}
-        >
-          {!startScanning ? "Start Scanning" : "Stop Scanning"}
-        </button>
+        <button onClick={handleStartCamera}>Start Scanning</button>
       </div>
     </div>
   );
