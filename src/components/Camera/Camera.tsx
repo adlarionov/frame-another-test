4de638764107fa3f57d9208286af35ac9860c9ac
payne3105 <payne3105@gmail.com>
2023-11-26 11:54:55 +0300

    test

--- src/components/Camera/Camera.tsx
+++ src/components/Camera/Camera.tsx
@@ -1,34 +1,12 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Frame from "../Frame/Frame";
 
 export default function Camera() {
   const videoRef = useRef<HTMLVideoElement>(null);
   // const [startScanning, setStartScanning] = useState<boolean>(false);
-  const [stream, setStream] = useState<MediaStream | null>(null);
+  // const [stream, setStream] = useState<MediaStream | null>(null);
 
   const [facingMode, setFacingMode] = useState<string>("environment");
-
-  const handleStartCamera = async () => {
-    console.log("here camera");
-    try {
-      // if (stream) {
-      //   stream.getVideoTracks().forEach((track) => track.stop());
-      //   console.log("here camera 2", facingMode);
-      // }
-      const mediaDevices = await navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: { facingMode, aspectRatio: 1 },
-      });
-      setStream(mediaDevices);
-    } catch (error) {
-      console.error(error);
-      return;
-    }
-    if (videoRef.current && stream) {
-      videoRef.current.srcObject = stream;
-      videoRef.current.play();
-    }
-  };
 
   const changeFacingMode = () => {
     if (facingMode === "user") {
@@ -36,8 +14,28 @@ export default function Camera() {
     } else {
       setFacingMode("user");
     }
-    handleStartCamera();
   };
+
+  useEffect(() => {
+    const handleStartCamera = async () => {
+      console.log("here camera");
+      try {
+        const mediaDevices = await navigator.mediaDevices.getUserMedia({
+          audio: false,
+          video: { facingMode },
+        });
+        // setStream(mediaDevices);
+        if (videoRef.current) {
+          videoRef.current.srcObject = mediaDevices;
+          videoRef.current.play();
+        }
+      } catch (error) {
+        console.error(error);
+        return;
+      }
+    };
+    handleStartCamera();
+  }, [facingMode]);
 
   // useEffect(() => {
   //   const startScanningFunc = async () => {
@@ -68,7 +66,7 @@ export default function Camera() {
         width="99%"
         height="500"
       />
-      {videoRef.current?.playsInline && <Frame video={videoRef.current} />}
+      {videoRef.current && <Frame video={videoRef.current} />}
       <div
         style={{
           display: "flex",
@@ -76,10 +74,7 @@ export default function Camera() {
           alignItems: "center",
         }}
       >
-        {stream && (
-          <button onClick={changeFacingMode}>Facing Mode {facingMode}</button>
-        )}
-        <button onClick={handleStartCamera}>Start Scanning</button>
+        <button onClick={changeFacingMode}>Facing Mode {facingMode}</button>
       </div>
     </div>
   );
